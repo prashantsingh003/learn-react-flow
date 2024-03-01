c16e2e0c25c66cdadc9f29fe778103b185f22e6b
Prashant Singh <53073860+prashantsingh003@users.noreply.github.com>
2024-03-01 12:33:51 +0530

    edges with redux

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -17,6 +17,7 @@ export function Calculator() {
 	const nodeTypes = useMemo(() => ({ add: Add, sub: Subtract, mul: Multiply, num: NumNode, result: Result }), []);
 	const edgeTypes = useMemo(() => ({ 'custom-edge': ColoredEdge }), [])
 	const dispatch = useDispatch()
+	const { getNodes, getEdges } = useReactFlow();
 	// const defaultEdgeOptions = useMemo(()=>({ type: 'edgeTypes'}),[])
 
 	const rdxNodes = useSelector((state => state.nodes))
@@ -48,30 +49,29 @@ export function Calculator() {
 	};
 	useEffect(() => {
 		let res = 0;
-		const nodes=rdxNodes
-		let node = nodes.find(el => el.type = 'num')
-		console.log(node)
-		// if (!node) return;
-		// const getNextNodeId = (sourceId: String) => rdxEdges.find((el) => el.source == sourceId)
-		// while (node) {
-		// 	switch (node.type) {
-		// 		case 'mul':
-		// 			res *= node.data.val
-		// 			break;
-		// 		case 'add':
-		// 			res += node.data.val
-		// 			break;
-		// 		case 'sub':
-		// 			res -= node.data.val
-		// 			break;
-		// 		default:
-		// 			res += node.data.val
-		// 			break;
-		// 	}
-		// 	const nextNodeId = getNextNodeId(node.id)?.target;
-		// 	node = rdxNodes.find((el) => el.id == nextNodeId)
-		// 	setChainResult(res)
-		// }
+		const currNodes=getNodes()
+		let node = currNodes.find(el => el.type = 'num')
+		if (!node) return;
+		const getNextNodeId = (sourceId: String) => rdxEdges.find((el) => el.source == sourceId)
+		while (node) {
+			switch (node.type) {
+				case 'mul':
+					res *= node.data.val
+					break;
+				case 'add':
+					res += node.data.val
+					break;
+				case 'sub':
+					res -= node.data.val
+					break;
+				default:
+					res += node.data.val
+					break;
+			}
+			const nextNodeId = getNextNodeId(node.id)?.target;
+			node = rdxNodes.find((el) => el.id == nextNodeId)
+			setChainResult(res)
+		}
 	}, [rdxEdges])
 
 	// Edge connect prevent self refrence
